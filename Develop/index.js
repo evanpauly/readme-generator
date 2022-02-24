3e9edabdcb44585f08fe732b4db1f57d73736522
Evan <evanpauly@hotmail.com>
2022-02-24 14:05:12 -0600

    starting to create questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,64 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project? (Required)',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the title of your project');
+                    return false;
+                }
+            }
+        }, 
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Give a brief description of your project. (Required)',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the description of your project');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'table of contents',
+            message: 'Provide a Table of Contents (Required)',
+            validate: tableInput => {
+                if (tableInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a table of contents');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Provide installation instructions (Required)',
+            validate: installInput => {
+                if (installInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the title of your project');
+                    return false;
+                }
+            }
+        },
+    ]);
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,3 +68,5 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+questions();
